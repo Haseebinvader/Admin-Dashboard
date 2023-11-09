ef93352610f1a220657747e3932c345911733969
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 <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>
2023-11-10 03:09:31 +0500

    apis integrated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,35 +1,56 @@
-import Chart from '../../components/chart/Chart'
-import Featured from '../../components/featured/Featured'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import Widget from '../../components/widgets/Widget'
-import Table from '../../components/table/Table'
-import './home.scss'
+import Chart from "../../components/chart/Chart";
+import Featured from "../../components/featured/Featured";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widgets/Widget";
+import Table from "../../components/table/Table";
+import "./home.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <Sidebar />
-            <div className="homeContainer">
-                <Navbar />
-                
-                <div className="widgets">
-                    <Widget type="user" />
-                    <Widget type="order" />
-                    <Widget type="earning" />
-                    <Widget type="balance" />
-                </div>
-                <div className="charts">
-                    <Featured />
-                    <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
-                </div>
-                <div className="listContainer">
-                    <div className="listTitle">Recent Users</div>
-                    <Table />
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-export default Home
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/admin/analytics")
+      .then((response) => {
+        console.log(response);
+        setData(response.data.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching teacher data:", error);
+        setIsLoading(false);
+      });
+  }, [data]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+
+        <div className="widgets">
+          <Widget type="user" dataanl={data?.allstudentsanl} />
+          <Widget type="order" dataanl={data?.allteachersanl} />
+          <Widget type="earning" dataanl={data?.allparentsanl} />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Recent Users</div>
+          <Table dataanl={data?.docs} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
