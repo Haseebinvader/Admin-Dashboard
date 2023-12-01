60e4f83a79c8888e52f7d6f0529729a46a161a92
Haseebinvader <rhaseeb741@gmail.com>
2023-12-01 16:01:49 +0500

    upaates

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -9,98 +9,6 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 const List = ({ dataanl }) => {
-  // const rows = [
-  //   {
-  //     id: 1143155,
-  //     product: "Teacher",
-  //     img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "John Smith",
-  //     date: "1 March",
-  //     amount: 785,
-  //     method: "Cash on Delivery",
-  //     status: "Approved",
-  //   },
-  //   {
-  //     id: 2235235,
-  //     product: "Student",
-  //     img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Michael Doe",
-  //     date: "1 March",
-  //     amount: 900,
-  //     method: "Online Payment",
-  //     status: "Pending",
-  //   },
-  //   {
-  //     id: 2342353,
-  //     product: "Teacher",
-  //     img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "John Smith",
-  //     date: "1 March",
-  //     amount: 35,
-  //     method: "Cash on Delivery",
-  //     status: "Pending",
-  //   },
-  //   {
-  //     id: 2357741,
-  //     product: "Teacher",
-  //     img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Jane Smith",
-  //     date: "1 March",
-  //     amount: 920,
-  //     method: "Online",
-  //     status: "Approved",
-  //   },
-  //   {
-  //     id: 2342355,
-  //     product: "Student",
-  //     img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Harold Carol",
-  //     date: "1 March",
-  //     amount: 2000,
-  //     method: "Online",
-  //     status: "Pending",
-  //   },
-  //   {
-  //     id: 2357741,
-  //     product: "Teacher",
-  //     img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Jane Smith",
-  //     date: "1 March",
-  //     amount: 920,
-  //     method: "Online",
-  //     status: "Approved",
-  //   },
-  //   {
-  //     id: 2342355,
-  //     product: "Student",
-  //     img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Harold Carol",
-  //     date: "1 March",
-  //     amount: 2000,
-  //     method: "Online",
-  //     status: "Pending",
-  //   },
-  //   {
-  //     id: 2357741,
-  //     product: "Parent",
-  //     img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Jane Smith",
-  //     date: "1 March",
-  //     amount: 920,
-  //     method: "Online",
-  //     status: "Approved",
-  //   },
-  //   {
-  //     id: 23423553,
-  //     product: "Parent",
-  //     img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-  //     customer: "Parent",
-  //     date: "1 March",
-  //     amount: 2000,
-  //     method: "Online",
-  //     status: "Pending",
-  //   },
-  // ];
   return (
     <TableContainer component={Paper} className="table">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -133,21 +41,19 @@ const List = ({ dataanl }) => {
               <TableCell>{row.teachingsExperience}</TableCell>
               <TableCell>{row.dateOfBirth}</TableCell>
               <TableCell>{row.phone}</TableCell>
-           
+
               <TableCell className="tableCell">
                 <span
-                  className={`status ${
-                    row.profile_completeion ? "Approved" : "Pending"
-                  }`}
+                  className={`status ${row.profile_completeion ? "Approved" : "Pending"
+                    }`}
                 >
                   {row.profile_completeion ? "Completed" : "Pending"}
                 </span>
               </TableCell>
               <TableCell className="tableCell">
                 <span
-                  className={`status ${
-                    row.profileverify ? "Approved" : "Pending"
-                  }`}
+                  className={`status ${row.profileverify ? "Approved" : "Pending"
+                    }`}
                 >
                   {row.profileverify ? "Verified" : "Unverified"}
                 </span>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -42,7 +42,6 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          {/* <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} /> */}
         </div>
         <div className="listContainer">
           <div className="listTitle">Recent Users</div>
