a789633961a9a385de9e175d67140e9b9c35e990
akashahmad <engr.akashahmad@gmail.com>
2023-12-06 01:07:54 +0500

    users analytics fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,32 @@
 // import Chart from "../../components/chart/Chart";
 // import Featured from "../../components/featured/Featured";
-import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Widget from "../../components/widgets/Widget";
-import Table from "../../components/table/Table";
-import "./home.scss";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Navbar from "../../components/navbar/Navbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Widget from "../../components/widgets/Widget"
+import Table from "../../components/table/Table"
+import "./home.scss"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const Home = () => {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     axios
       .get("/admin/analytics")
       .then((response) => {
-        console.log(response);
-        setData(response.data.data);
-        setIsLoading(false);
+        console.log(response)
+        setData(response.data.data)
+        setIsLoading(false)
       })
       .catch((error) => {
-        console.error("Error fetching teacher data:", error);
-        setIsLoading(false);
-      });
-  }, []);
+        console.error("Error fetching teacher data:", error)
+        setIsLoading(false)
+      })
+  }, [])
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <p>Loading...</p>
   }
   return (
     <div className="home">
@@ -35,9 +35,9 @@ const Home = () => {
         <Navbar />
 
         <div className="widgets">
-           <Widget type="user" dataanl={data?.allstudentsanl} />
+          <Widget type="user" dataanl={data?.allstudentsanl} />
           <Widget type="order" dataanl={data?.allteachersanl} />
-          <Widget type="earning" dataanl={data?.allparentsanl} /> 
+          <Widget type="earning" dataanl={data?.allparentsanl} />
           {/* <Widget type="balance" /> */}
         </div>
         <div className="charts">
@@ -46,11 +46,13 @@ const Home = () => {
         </div>
         <div className="listContainer">
           <div className="listTitle">Recent Users</div>
-          <Table dataanl={data?.docs} />
+          <Table
+            dataanl={data?.docs?.filter((data) => data?.userType !== "admin")}
+          />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
